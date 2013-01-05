f4552c83897a6dd2dd19f3f2c00dab26cb29a2fb
João Lemos <joao.lemos@codequarks.org>
2013-01-05 12:11:58 +0000

    Restored the star

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,9 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("+"), DEAD_CELL(".");
-
-    //Hi :)
+    LIVE_CELL("*"), DEAD_CELL(".");
     
     private String symbol;
 
